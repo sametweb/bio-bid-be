b0240590e0806c31c4165a34787ba952508b4661
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-26 16:09:18 -0500

    added in codeclimate badges for testing and maintainability

--- index.js
+++ index.js
@@ -5,49 +5,47 @@ const {prisma} = require('./prisma/generated/prisma-client');
 const typeDefs = gql`
    type Query {
         hello: String!,
-        studies: [Study!]!
+        getAllStudies: [Study!]!
     },
-    type studies {
-        id: ID!
-        name: String!
-        area: Int!
-        status: String!
-        company: Company!
-    },
+
     type Company {
-        id: ID!
+        id: ID! 
         name: String!
         studies: [Study!]
         bids: [Bid!]
     },
+
+    type Bid {
+        id: ID! 
+        company: Company! 
+        bid_amount: Float!
+        is_approved: Boolean! 
+        study: Study!
+    },
+
     type Study {
-        id: ID!
-        name: String!
+        id: ID! 
+        name: String! 
         area: String!
         phase: Int!
         status: String!
-        company: Company!
-    },
-    type Bid {
-        id: ID!
-        company: Company!
-        bid_amount: Float!
-        is_approved: Boolean!
-        study: Study!
+        company: Company! 
     }
 `;
 
 const resolvers = {
     Query: {
         hello: () => "Hello World",
-        studies: async (parent, args, {prisma}) => {
-            const studies = await prisma.studies({});
-            return studies;
-        }
+       getAllStudies: async (parent, args, {prisma}, info) => {
+        const studies = await prisma.studies();
+        return studies;
+       }
     }
 }
 
-const server = new ApolloServer({typeDefs, resolvers, context: {prisma}});
+const server = new ApolloServer({typeDefs, resolvers, context: ({req}) =>{
+    return {prisma}
+}});
 
 server.listen({port: process.env.PORT || 5000}).then(({url}) =>{
     console.log(`Server is running at ${url}`);
