469fabe4b84b681369f685f0b9656ebeac600110
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-04-11 11:44:37 -0500

    resolver for getting a list of companies with all of their studies works

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -11,6 +11,11 @@ const resolvers = {
     Study: {
         company: (parent, args, {prisma}, info) => {
          return prisma.study({id: parent.id}).company();
+        },
+    },
+    Company: {
+        studies: (parent, args, {prisma}, info) => {
+            return prisma.studies();
         }
     }
 }
