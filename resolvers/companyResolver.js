561b2652f4916fe868042bdb2d38c597bbe9cb1a
sametweb <sametmutevellioglu@gmail.com>
2020-05-21 03:18:24 -0700

    Fields added, a bug on asyncForEach fixed

--- resolvers/companyResolver.js
+++ resolvers/companyResolver.js
@@ -1,7 +1,7 @@
 const asyncForEach = async (array, finder, creator) => {
   for (let i = 0; i < array.length; i++) {
-    const created = await finder({ name: array[i].name });
-    if (!created) await creator(array[i]);
+    const found = await finder({ name: array[i].name });
+    if (!found) await creator(array[i]);
   }
 };
 
@@ -16,30 +16,85 @@ module.exports = {
   },
   Mutation: {
     createCompany: async (parent, args, { prisma }, info) => {
-      const { name, services, specialties } = args;
+      const {
+        name,
+        logoURL,
+        website,
+        linkedin,
+        overview,
+        headquarters,
+        companySize,
+        services,
+        specialties,
+      } = args;
 
       // If the service is not in DB, add it
-      await asyncForEach(services, prisma.service, prisma.createService);
+      if (services) {
+        await asyncForEach(services, prisma.service, prisma.createService);
+      }
+      // If the specialty is not in DB, add it
+      if (specialties) {
+        await asyncForEach(
+          specialties,
+          prisma.specialty,
+          prisma.createSpecialty
+        );
+      }
 
       return await prisma.createCompany({
         name,
+        logoURL,
+        website,
+        linkedin,
+        overview,
+        headquarters,
+        companySize,
         services: { connect: services },
+        specialties: { connect: specialties },
       });
     },
     updateCompany: async (parent, args, { prisma }, info) => {
-      const { updated_name, updated_services, name } = args;
+      const {
+        updated_name,
+        updated_logoURL,
+        updated_website,
+        updated_linkedin,
+        updated_overview,
+        updated_headquarters,
+        updated_companySize,
+        updated_services,
+        updated_specialties,
+        name,
+      } = args;
 
       // If the service is not in DB, add it
-      await asyncForEach(
-        updated_services,
-        prisma.service,
-        prisma.createService
-      );
+      if (updated_services) {
+        await asyncForEach(
+          updated_services,
+          prisma.service,
+          prisma.createService
+        );
+      }
+
+      if (updated_specialties) {
+        await asyncForEach(
+          updated_specialties,
+          prisma.specialty,
+          prisma.createSpecialty
+        );
+      }
 
       return await prisma.updateCompany({
         data: {
-          name: updated_name || name,
+          name: updated_name,
+          logoURL: updated_logoURL,
+          website: updated_website,
+          linkedin: updated_linkedin,
+          overview: updated_overview,
+          headquarters: updated_headquarters,
+          companySize: updated_companySize,
           services: { connect: updated_services },
+          specialties: { connect: updated_specialties },
         },
         where: { name },
       });
--- typeDefs/company.js
+++ typeDefs/company.js
@@ -9,24 +9,44 @@ module.exports = gql`
   extend type Mutation {
     createCompany(
       name: String!
+      logoURL: String
+      website: String
+      linkedin: String
+      overview: String
+      headquarters: String
+      companySize: CompanySize
       services: [ServiceInput]
       specialties: [SpecialtyInput]
     ): Company!
+
     updateCompany(
       name: String! # FILTER
       updated_name: String
+      updated_logoURL: String
+      updated_website: String
+      updated_linkedin: String
+      updated_overview: String
+      updated_headquarters: String
+      updated_companySize: CompanySize
       updated_services: [ServiceInput]
       updated_specialties: [SpecialtyInput]
     ): Company!
+
     deleteCompany(name: String!): Company!
   }
 
   type Company {
     id: ID
     name: String!
-    studies: [Study!]
+    logoURL: String
+    website: String
+    linkedin: String
+    overview: String
+    headquarters: String
+    companySize: CompanySize
     services: [Service!]
     specialties: [Specialty!]
+    studies: [Study!]
     bids: [Bid!]
   }
 
@@ -36,5 +56,17 @@ module.exports = gql`
 
   input SpecialtyInput {
     name: String!
+  }
+
+  enum CompanySize {
+    A # Self-employed
+    B # 1-10 employees
+    C # 11-50 employees
+    D # 51-200 employees
+    E # 201-500 employees
+    F # 501-1000 employees
+    G # 1001-5000 employees
+    H # 5001-10,000 employees
+    I # 10,001+ employees
   }
 `;
