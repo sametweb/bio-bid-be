aea21a8d8e908d144d26cfa40dbe35ddbc241de5
sametweb <sametmutevellioglu@gmail.com>
2020-06-12 01:39:42 -0700

    test: therapeuticResolver

--- tests/therapeuticResolver.test.js
+++ tests/therapeuticResolver.test.js
@@ -13,16 +13,27 @@ const dummyTherapeutics = {
 };
 
 const prisma = {
+  $exists: { therapeutic: jest.fn() },
   therapeutics: jest.fn(() => dummyTherapeutics),
   therapeutic: jest.fn(() => dummyTherapeutic),
-  createtherapeutic: jest.fn(() => dummyTherapeutic),
-  updatetherapeutic: jest.fn(() => dummyTherapeutic),
+  searchTherapeutics: jest.fn(() => dummyTherapeutics),
+  createTherapeutic: jest.fn(() => dummyTherapeutic),
+  updateTherapeutic: jest.fn(() => dummyTherapeutic),
   deletetherapeutic: jest.fn(() => dummyTherapeutic),
 };
 
 describe("Query", () => {
   const therapeutic = jest.spyOn(therapeuticResolver.Query, "therapeutic");
   const therapeutics = jest.spyOn(therapeuticResolver.Query, "therapeutics");
+  const searchTherapeutics = jest.spyOn(
+    therapeuticResolver.Query,
+    "searchTherapeutics"
+  );
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   const params = [{}, {}, { prisma }, {}];
 
   describe("therapeutics()", () => {
@@ -44,6 +55,55 @@ describe("Query", () => {
       expect(prisma.therapeutic).toHaveBeenCalledWith(
         expect.objectContaining({ name: "Company" })
       );
+    });
+  });
+  describe("searchTherapeutics()", () => {
+    const params = [{}, { search: "as" }, { prisma }, {}];
+
+    it("throws an error if search length is less than 3 characters", async () => {
+      const request = async () => await searchTherapeutics(...params);
+      await expect(() => request()).rejects.toThrow(
+        "Please enter a search term at least 3 characters"
+      );
+      expect(prisma.therapeutics).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Mutation", () => {
+  const createTherapeutic = jest.spyOn(
+    therapeuticResolver.Mutation,
+    "createTherapeutic"
+  );
+  const updateTherapeutic = jest.spyOn(
+    therapeuticResolver.Mutation,
+    "updateTherapeutic"
+  );
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("createTherapeutic()", () => {
+    it("throws an error if there is a therapeutic area with same name", async () => {
+      const params = [{}, { name: "Company" }, { prisma }, {}];
+      prisma.$exists.therapeutic.mockImplementation(() => true);
+
+      await expect(createTherapeutic(...params)).rejects.toThrow(
+        `There is a therapeutic area named '${
+          params[1].name
+        }' already, please enter a different name.`
+      );
+      expect(prisma.createTherapeutic).not.toHaveBeenCalled();
+    });
+
+    it("calls prisma.createTherapeutic()", async () => {
+      const params = [{}, { name: "Company" }, { prisma }, {}];
+      prisma.$exists.therapeutic.mockImplementation(() => false);
+
+      await createTherapeutic(...params);
+
+      expect(prisma.createTherapeutic).toHaveBeenCalledTimes(1);
     });
   });
 });
